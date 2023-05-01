018c13495a27dcff281e4e56915259917bd2a8a4
ThameurBenAbdallah <bathameur@gmail.com>
2023-05-01 17:06:42 +0100

    First commit

---
+++ src/app/fruits/fruits.ts
@@ -0,0 +1,6 @@
+export interface Fruits {
+    id: number;
+    Name: string;
+    Price: number;
+    Quantity: number;
+}
---
+++ src/app/fruits/home/home.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { Fruits } from '../fruits';
+import { FruitsService } from '../fruits.service';
+declare var window: any;
+
+@Component({
+  selector: 'crud-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  allFruits: Fruits[] = [];
+  deleteModal: any;
+  idTodelete: number = 0;
+
+
+  constructor(private fruitService: FruitsService){}
+
+  ngOnInit(): void {
+    this.get();
+    
+    this.deleteModal = new window.bootstrap.Modal(
+      document.getElementById('deleteModal')
+    );
+ 
+    
+  }
+
+  get() {
+    this.fruitService.get().subscribe((data) => {
+      this.allFruits = data;
+    });
+  }
+
+  openDeleteModal(id: number) {
+    this.idTodelete = id;
+    this.deleteModal.show();
+  }
+
+  delete() {
+    this.fruitService.delete(this.idTodelete).subscribe({
+      next: (data) => {
+        this.allFruits = this.allFruits.filter(_ => _.id != this.idTodelete)
+        this.deleteModal.hide();
+      },
+    });
+  }
+}
